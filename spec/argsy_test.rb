179078bc01b55afd19c4f926795f4e0f7db2eb7e
kolotaev <kolotaev@tut.by>
2019-02-04 16:06:58 +0300

    POC implementation

---
+++ spec/argsy_test.rb
@@ -0,0 +1,24 @@
+require_relative "../argsy"
+require "test/unit"
+ 
+class TestArgsy < Test::Unit::TestCase
+
+  def test_one_command
+    actual = {}
+    argsy = Argsy.new do |a|
+      a.command :list, "foo bar" do |c|
+        c.action do |opts|
+          $actual = opts
+        end
+        c.options do |op|
+          op.on("-d", "--detached", "List available docker-composes") { |o| c.opts[:detached] = o }
+          op.on("-e", "--ext [EXTENSION]", "List available docker-composes") { |o| c.opts[:extension] = o }
+        end
+      end
+    end
+    # ARGV = %w[list]
+    argsy.run!
+    assert_equal({}, actual)
+  end
+
+end
